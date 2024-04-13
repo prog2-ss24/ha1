6657994c2ff869427794bfa9dc573f1a76d1be72
Jerome Pincus <Jerome.Pincus@Student.HTW-Berlin.de>
2024-04-13 14:38:14 +0200

    Imports

---
+++ app/src/test/java/htw/berlin/prog2/ha1/JCalculatorTest.java
@@ -0,0 +1,8 @@
+package htw.berlin.prog2.ha1;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class JCalculatorTest {
+
+}
