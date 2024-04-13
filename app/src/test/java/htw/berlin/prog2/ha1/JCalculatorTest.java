bf47e865fd9c61c3e6b699881218aff36f5816df
Jerome Pincus <Jerome.Pincus@Student.HTW-Berlin.de>
2024-04-13 14:42:47 +0200

    Teilaufgabe 1: First green test!

--- app/src/test/java/htw/berlin/prog2/ha1/JCalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/JCalculatorTest.java
@@ -5,4 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class JCalculatorTest {
 
+    @Test
+    void testChangeSignPositiveToNegatie() {
+        Calculator calculator = new Calculator();
+
+        calculator.pressDigitKey(9);
+        calculator.pressNegativeKey();
+
+        String expected = "-9";
+        String actual = calculator.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 }
