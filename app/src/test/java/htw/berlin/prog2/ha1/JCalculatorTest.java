145ac8d9fa97384eb62ba5fe04d9abf77d63d93f
Jerome Pincus <Jerome.Pincus@Student.HTW-Berlin.de>
2024-04-13 16:41:32 +0200

    Teilaufgabe 2: Two red tests!

--- app/src/test/java/htw/berlin/prog2/ha1/JCalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/JCalculatorTest.java
@@ -1,8 +1,10 @@
 package htw.berlin.prog2.ha1;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JCalculatorTest {
 
     @Test
@@ -13,6 +15,37 @@ public class JCalculatorTest {
         calculator.pressNegativeKey();
 
         String expected = "-9";
+        String actual = calculator.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testLimitDisplayToSixteenDigits() {
+        Calculator calculator = new Calculator();
+
+        for (int i = 0; i < 17; i++) {
+
+            calculator.pressDigitKey(9);
+
+        }
+
+        String expected = "9999999999999999";
+        String actual = calculator.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
+    //-------------------------------------------------------------------------------
+    @Test
+    void testNegativeWithZero() {
+        Calculator calculator = new Calculator();
+
+        calculator.pressNegativeKey();
+
+
+        String expected = "0";
         String actual = calculator.readScreen();
 
         assertEquals(expected, actual);
