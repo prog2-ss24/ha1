4d2affbddfd9710810fa944107d2a059635ef916
enesaslnts <Enes.Aslantas@Student.HTW-Berlin.de>
2024-04-16 15:40:39 +0200

    1. roter Test (korrigiert)

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -125,23 +125,4 @@ class CalculatorTest {
         assertEquals(expected, actual);
 
     }
-
-    @Test
-    @DisplayName("")
-    void addDiv() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(1);
-        calc.pressDigitKey(0);
-        calc.pressBinaryOperationKey("/");
-        calc.pressDigitKey(2);
-        calc.pressBinaryOperationKey("-");
-        calc.pressDigitKey(3);
-        calc.pressEqualsKey();
-
-        String expected = "2";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
 }
