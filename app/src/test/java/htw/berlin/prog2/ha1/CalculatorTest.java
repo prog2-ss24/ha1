4c78ce8f74d221e37c463babbc81505229212cfa
Pascal Noack <pascal7603@gmail.com>
2024-04-17 13:26:11 +0200

    Teilaufgabe 2 --> 2 Rote testfälle

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -91,8 +91,8 @@ class CalculatorTest {
 
     //TODO hier weitere Tests erstellen
 
- @Test
- @DisplayName("should display result after substracting two positive multi-digit numbers")
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
     void testPositiveSubtraction() {
         Calculator calc = new Calculator();
         calc.pressDigitKey(1);
@@ -102,6 +102,75 @@ class CalculatorTest {
         calc.pressEqualsKey();
 
         assertEquals("10", calc.readScreen());
- }
+    }
+
+    @Test
+    @DisplayName("should display 0 after multiply with 0")
+    void testZero() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressDigitKey(0);
+        calc.pressDigitKey(1);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        assertEquals("0", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("should display the subtraction after pressing clear key")
+    void testClearKey() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("-");
+        calc.pressClearKey();
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        assertEquals("10", calc.readScreen());
+    }
+
+    @Test
+    @DisplayName("should display the result after adding two negative multi-digit numbers")
+    void testNegativeAddition() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(7);
+        calc.pressNegativeKey();
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(7);
+        calc.pressNegativeKey();
+        calc.pressEqualsKey();
+
+        assertEquals("-14", calc.readScreen());
+    }
+    @Test
+    @DisplayName("should display the result after dividing with a negative number")
+    void testNegativeDivision() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(4);
+        calc.pressDigitKey(7);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(7);
+        calc.pressNegativeKey();
+        calc.pressEqualsKey();
+
+        assertEquals("-6.7142857", calc.readScreen());
+    }
+@Test
+    @DisplayName("should display the result after using % operator")
+    void testOperator() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(3);
+        calc.pressUnaryOperationKey("%");
+        calc.pressEqualsKey();
+
+        assertEquals("0.23", calc.readScreen());
+
 }
+}
+
+
 
