bc66e3638662ddaca8ecd83fb584d81a7e516370
Mirav Ahmad <Mirav.Ahmad@Student.HTW-Berlin.de>
2024-04-19 18:04:24 +0200

    Teilausgabe 2 (2.RoteTest mit fehler)

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -132,6 +132,23 @@ class CalculatorTest {
 
         assertEquals(expected,actual);
 }
+    /**
+     * Testfall für die Anzeige eines Fehlers bei der Division durch Null.
+     */
+
+    @Test
+    @DisplayName("should show error on division by zero")
+    void testInversionByZero() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("1/x");
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected,actual);
+}
 
 }
 
