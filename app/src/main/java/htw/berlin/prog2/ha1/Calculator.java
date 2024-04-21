5eaf8d6c95222cb779f7b38b10a0bff3809db19d
Jabsi48 <Niklas.Jabs@student.htw-berlin.de>
2024-04-21 18:02:51 +0200

    FIX roter Test2 Kettenrechnung mit gleichen Operatoren

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -148,7 +148,7 @@ public class Calculator {
             case "/" -> latestValue / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
-        screen = String.valueOf(result);
+        screen = Double.toString(result);
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
