0301189a4d113e7120686685808739d1c0811ff1
Leif <leifstoehr333@gmail.com>
2024-04-23 13:19:18 +0200

    2 bugfixes for redtest last

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -79,17 +79,20 @@ public class Calculator {
     public void pressUnaryOperationKey(String operation) {
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
-        var result = switch(operation) {
+        var result = switch (operation) {
             case "√" -> Math.sqrt(Double.parseDouble(screen));
             case "%" -> Double.parseDouble(screen) / 100;
             case "1/x" -> 1 / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.equals("NaN")) screen = "Error";
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if (screen.equals("NaN")) screen = "Error";
+        if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if ("Infinity".equals(screen)) { //Infinity bedeutet Unendlichkeit - bei Inversion(Kehrwert) von  ist mit screen - Error angezeigt
+            screen = "Error";
 
 
+        }
     }
 
     /**
