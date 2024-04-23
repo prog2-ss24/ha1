5d63707c45e603648acdc443a78c912a51057068
s0579111 <s0579111@htw-berlin.de>
2024-04-23 12:47:26 +0200

    Teilaufgabe 2 - roter Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -103,6 +103,43 @@ class CalculatorTest {
     
         assertEquals(expected, actual);
     }
+//Teilaufgabe 2
+    @Test
+    @DisplayName("should display error when trying to invert zero")
+    void testInvertingZero() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("1/x");
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("should display error when exceeding maximum screen capacity")
+    void testMaxScreenCapacity() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(3);
+        calc.pressDigitKey(4);
+        calc.pressDigitKey(5);
+        calc.pressDigitKey(6);
+        calc.pressDigitKey(7);
+        calc.pressDigitKey(8);
+        calc.pressDigitKey(9);
+        calc.pressDigitKey(0);
+        calc.pressDigitKey(1); // sollte ignoriert werden
+
+        String expected = "1234567890";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 
 }
 
