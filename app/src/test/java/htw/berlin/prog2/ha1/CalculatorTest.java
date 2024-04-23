1d222e6dc7e2b71ee90143aa8ec692867d232c51
Hannesson1 <Johannes.Kisthardt@Student.HTW-Berlin.de>
2024-04-23 16:08:06 +0200

    Neuer roter zweiter Testfall

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -124,7 +124,20 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    @DisplayName("should not allow inverting zero")
+    void testInvertZero() {
+        Calculator calc = new Calculator();
 
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("1/x");
+
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 
 }
 
