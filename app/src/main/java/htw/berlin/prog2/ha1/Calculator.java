9bdec8b67b7a7f39080748df2a75b72d28e1e42c
MohamadSallakhou <s0592381@htw-berlin.de>
2024-04-23 12:58:26 +0200

    teilaufgabe 2 2 roteTest gefixt

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -84,7 +84,7 @@ public class Calculator {
         if(screen.equals("NaN")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
         if(screen.equals("Infinity")) screen = "Error";
-      //  if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
 
 
 
