3c7eff4150899f66b4cf1c5a116c7db8465d717f
meli99b <167767677+meli99b@users.noreply.github.com>
2024-04-27 20:46:43 +0200

    New Green Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -89,10 +89,9 @@ class CalculatorTest {
     }
 
 
-    //TODO hier weitere Tests erstellen
+
 }
-git commit -m     "New Green Test Percentage Calculation"
-git log 
+ 
 
     @Test
     @DisplayName("should display result after multiplying two numbers with the percentage operation key")
@@ -112,86 +111,11 @@ git log
         String actual = calc.readScreen();
         assertEquals(expected, actual);
     }
-}
-git commit -m  "New Red Test Input Validation Error"
-git log
-    
-    @Test
-    @DisplayName("should ensure screen remains unchanged, when there is an invalid input") 
-    void testInputValidationError() {
-       
-        Calculator calculator = new Calculator();
 
-        try {
-            calculator.pressDigitKey('A');
-            fail("Expected IllegalArgumentException was not thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("0", calculator.readScreen()); 
-            assertEquals(expected, actual);
-          
-        
-}
-       
-git commit -m "Fix for the New Red test Input Validation Error"
-git log    
-
-    
-    public void pressDigitKey(int digit) {
-        if (digit < 0 || digit > 9) {
-         
-            System.out.println("Invalid Input");
-            return; 
-        }
-        
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
-        screen = screen + digit;
-    }
 }
 
-
-git commit -m "New Red Test Round Off Error"
-git log    
-
-    @Test
-    @DisplayName("should be able to give a rounded result")
-    void testRoundOffError() {
     
-        Calculator calculator = new Calculator();
-
-       
-        calculator.pressDigitKey(1);
-        calculator.pressDotKey(.); 
-        calculator.pressDigitKey(0);
-        calculator.pressDigitKey(0); 
-        calculator.pressBinaryOperationKey("/"); 
-        calculator.pressDigitKey(3); 
-        calculator.pressEqualsKey(); 
-
-        
-        assertEquals("0.3333333333", calculator.readScreen()); 
-        assertEquals(expected, actual); 
-}
 
 
-git commit -m "Fix for the New Red Test Round Off Error"
-git log    
-
-import java.text.DecimalFormat;
-
-  class Calculator {
-    
-    
-    public void pressEqualsKey() {
-        double result = switch(latestOperation) {
-           
-            default -> throw new IllegalArgumentException();
-        };
-        
-      
-        DecimalFormat df = new DecimalFormat("#.##########");
-        screen = df.format(result);
-    }
-}
-
-git push origin master 
+ 
     
