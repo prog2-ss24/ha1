e354fd5e5f1bafd4003cab5fb55bcb2590127e74
MyLinhChu <linhchu97@gmail.com>
2024-04-28 01:24:15 +0200

    Bugfix 1. roter Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if(screen.equals("0") || latestValue == Double.parseDouble(screen) ) screen = "";
 
         screen = screen + digit;
     }
@@ -60,8 +60,14 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
-        latestValue = Double.parseDouble(screen);
-        latestOperation = operation;
+        if(!latestOperation.equals("")){
+            pressEqualsKey();
+            latestValue = Double.parseDouble(screen);
+        }
+        else {
+            latestValue = Double.parseDouble(screen);
+            latestOperation = operation;
+        }
     }
 
     /**
