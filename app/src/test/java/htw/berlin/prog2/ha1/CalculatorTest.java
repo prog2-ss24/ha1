ef878b07b25772b492e4263fc69c3e4e8569a123
meli99b <167767677+meli99b@users.noreply.github.com>
2024-04-27 18:38:01 +0200

    Update CalculatorTest.java

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -92,3 +92,62 @@ class CalculatorTest {
     //TODO hier weitere Tests erstellen
 }
 
+
+    @Test
+    @DisplayName("should display result after multiplying two numbers with the percentage operation key")
+    void testPercentageCalculation() {
+      
+        Calculator calc = new Calculator();
+
+       
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("x"); 
+        calc.pressDigitKey(5);
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("%");
+
+       
+        String expected = "3";
+        String actual = calc.readScreen();
+        assertEquals(expected, actual);
+    }
+}
+
+    @Test
+    @DisplayName("should ensure screen remains unchanged, when there is an invalid input") 
+    void testInputValidationError() {
+       
+        Calculator calculator = new Calculator();
+
+        try {
+            calculator.pressDigitKey('A');
+            fail("Expected IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("0", calculator.readScreen()); 
+            assertEquals(expected, actual);
+          
+        
+}
+
+    @Test
+    @DisplayName("should be able to give a rounded result")
+    void testRoundOffError() {
+    
+        Calculator calculator = new Calculator();
+
+       
+        calculator.pressDigitKey(1);
+        calculator.pressDotKey(.); 
+        calculator.pressDigitKey(0);
+        calculator.pressDigitKey(0); 
+        calculator.pressBinaryOperationKey("/"); 
+        calculator.pressDigitKey(3); 
+        calculator.pressEqualsKey(); 
+
+        
+        assertEquals("0.3333333333", calculator.readScreen()); 
+        assertEquals(expected, actual); 
+}
+
+
+
