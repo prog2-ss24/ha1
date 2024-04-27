a6b6096a4c682da1bc22f880bf9353030b1f7475
Steffan2024 <steffanvansantine@outlook.de>
2024-04-27 12:45:51 +0200

    4 Commit für neuer roter Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -81,20 +81,15 @@ public class Calculator {
     public void pressUnaryOperationKey(String operation) {
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
-        if (operation.equals("√") && latestValue < 0){ //Anfang
-            screen = "Error";
-        } else {                                       // Ende
-            var result = switch (operation) {
-                case "√" -> Math.sqrt(Double.parseDouble(screen));
-                case "%" -> Double.parseDouble(screen) / 100;
-                case "1/x" -> 1 / Double.parseDouble(screen);
-                default -> throw new IllegalArgumentException();
-            };
-            screen = Double.toString(result);
-            if (screen.equals("NaN")) screen = "Error";
-            if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
-        }
+        var result = switch(operation) {
+            case "√" -> Math.sqrt(Double.parseDouble(screen));
+            case "%" -> Double.parseDouble(screen) / 100;
+            case "1/x" -> 1 / Double.parseDouble(screen);
+            default -> throw new IllegalArgumentException();
+        };
+        screen = Double.toString(result);
+        if(screen.equals("NaN")) screen = "Error";
+        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
     /**
