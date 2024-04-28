68d5612a2046ecc3bce8bea0ee92172e555aaaae
David Kohlmann <david.kohlmann@student.htw-berlin.de>
2024-04-28 16:33:15 +0200

    Teilaufgabe 1

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -87,8 +87,23 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
-
-
+    
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after multiplying two positive multi-digit numbers")
+    void testPositiveMultiplication() {
+    Calculator calc = new Calculator();
+
+    calc.pressDigitKey(5);
+    calc.pressBinaryOperationKey("x");
+    calc.pressDigitKey(4);
+    calc.pressEqualsKey();
+
+    String expected = "20";
+    String actual = calc.readScreen();
+
+    assertEquals(expected, actual);
+    }
 }
 
