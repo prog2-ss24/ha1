4e2f162957b5dde88074e2bfdec93725b0db6944
Baehmmm <Jim-Philip.Sedlacek@Student.HTW-Berlin.de>
2024-04-28 12:53:14 +0200

    3. Commit für den Fix zu diesem Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -81,9 +81,12 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.equals("NaN")) screen = "Error";
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
+        if(screen.contains(".") && screen.length() > 12 && screen.contains("-") ){
+            screen = screen.substring(0, 11);
+        } else {
+            if(screen.equals("NaN")) screen = "Error";
+            if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        }
     }
 
     /**
