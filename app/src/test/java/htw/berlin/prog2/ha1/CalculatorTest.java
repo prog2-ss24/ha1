45112a1d3807b88da44303c481dd31c35c812b53
Tigrestar <ibemba98@gmail.com>
2024-04-28 12:02:40 +0200

    Nächste Teilaufgabe : 1 Test mit Fehlermeldung

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -106,5 +106,25 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("should display result after adding two positive multi-digit numbers with the multiplication with a positive digit number")
+    void testPositiveAdditionwiththreenumbers() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "43";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
