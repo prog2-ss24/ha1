00ae5433797febca6a26a6d72e5a01245030cb49
Melek Göggöz <melek.g.gs.11@gmail.com>
2024-04-28 20:04:42 +0200

    Teilaufgabe 2 + grün test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -120,23 +120,6 @@ void testEqualKey() {
 }
     // Teiaufgabe 2.2
 
-    @Test
-    @DisplayName("should display error when dividing zero by zero")
-    void testZeroDivision() {
-        // Arrange
-        Calculator calc = new Calculator();
-
-        // Act
-        calc.pressDigitKey(0); // Eingabe der ersten Zahl
-        calc.pressBinaryOperationKey("/"); // Auswahl der Division
-        calc.pressDigitKey(0); // Eingabe der zweiten Zahl
-        calc.pressEqualsKey(); // Ausführen der Berechnung
-
-        // Assert
-        String expected = "Error";
-        String actual = calc.readScreen();
-        assertEquals(expected, actual);
-    }
 
 
 
