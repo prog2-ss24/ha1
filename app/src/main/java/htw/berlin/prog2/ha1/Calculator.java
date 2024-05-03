54f12a54d5ae0a856d574f4e4084aa363cc2c546
Alexander Kroll <Alexander.Kroll@student.htw-berlin.de>
2024-05-03 13:23:21 +0200

    5.grüner test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -123,14 +123,18 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+        if (latestOperation.isEmpty()) return;
+        double currentScreenValue = Double.parseDouble(screen);
         var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
+            case "+" -> latestValue + currentScreenValue;
+            case "-" -> latestValue - currentScreenValue;
+            case "x" -> latestValue * currentScreenValue;
+            case "/" -> latestValue / currentScreenValue;
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
+        //latestOperation = "";
+        latestValue = currentScreenValue;
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
